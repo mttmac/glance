6745f2a653cc66fa42c39b0d4815a92b3fbf981c
Matt MacDonald <mattmacdc@gmail.com>
2019-01-26 18:44:49 +0000

    Create week 2 demo for image VAE examples

---
+++ VAE2D.py
@@ -0,0 +1,228 @@
+import os, math, time
+from pathlib import Path
+from datetime import date
+
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+from torchvision import transforms, datasets
+from torchvision.utils import make_grid
+from torch.utils.data import DataLoader
+
+from sklearn.metrics import roc_auc_score
+from itertools import product
+import scipy.stats as stats
+
+depth = 64      # initial depth to convolve channels into
+n_channels = 3  # number of channels (RGB)
+filt_size = 4   # convolution filter size
+stride = 2      # stride for conv
+pad = 1         # padding added for conv
+
+class VAE2D(nn.Module):
+    def __init__(self, img_size, n_latent=300):
+        
+        # Model setup
+        #############
+        super(VAE2D, self).__init__()
+        self.n_latent = n_latent
+        n = math.log2(img_size)
+        assert n == round(n), 'Image size must be a power of 2'  # restrict image input sizes permitted
+        assert n >= 3, 'Image size must be at least 8'           # low dimensional data won't work well
+        n = int(n)
+
+        # Encoder - first half of VAE
+        #############################
+        self.encoder = nn.Sequential()  
+        # input: n_channels x img_size x img_size
+        # ouput: depth x conv_img_size^2
+        # conv_img_size = (img_size - filt_size + 2 * pad) / stride + 1
+        self.encoder.add_module('input-conv', nn.Conv2d(n_channels, depth, filt_size, stride, pad,
+                                                        bias=True))
+        self.encoder.add_module('input-relu', nn.ReLU(inplace=True))
+        
+        # Add conv layer for each power of 2 over 3 (min size)
+        # Pyramid strategy with batch normalization added
+        for i in range(n - 3):
+            # input: depth x conv_img_size^2
+            # output: o_depth x conv_img_size^2
+            # i_depth = o_depth of previous layer
+            i_depth = depth * 2 ** i
+            o_depth = depth * 2 ** (i + 1)
+            self.encoder.add_module(f'pyramid_{i_depth}-{o_depth}_conv',
+                                    nn.Conv2d(i_depth, o_depth, filt_size, stride, pad, bias=True))
+            self.encoder.add_module(f'pyramid_{o_depth}_batchnorm',
+                                    nn.BatchNorm2d(o_depth))
+            self.encoder.add_module(f'pyramid_{o_depth}_relu',
+                                    nn.ReLU(inplace=True))
+        
+        # Latent representation
+        #######################
+        # Convolve the encoded image into the latent space, once for mu and once for logvar
+        max_depth = depth * 2 ** (n - 3)
+        self.conv_mu = nn.Conv2d(max_depth, n_latent, filt_size)      # return the mean of the latent space 
+        self.conv_logvar = nn.Conv2d(max_depth, n_latent, filt_size)  # return the log variance of the same
+        
+        
+        # Decoder - second half of VAE
+        ##############################
+        self.decoder = nn.Sequential()
+        # input: max_depth x conv_img_size^2 (8 x 8)  TODO double check sizes
+        # output: n_latent x conv_img_size^2 (8 x 8)
+        # default stride=1, pad=0 for this layer
+        self.decoder.add_module('input-conv', nn.ConvTranspose2d(n_latent, max_depth, filt_size, bias=True))
+        self.decoder.add_module('input-batchnorm', nn.BatchNorm2d(max_depth))
+        self.decoder.add_module('input-relu', nn.ReLU(inplace=True))
+    
+        # Reverse the convolution pyramids used in the encoder
+        for i in range(n - 3, 0, -1):
+            i_depth = depth * 2 ** i
+            o_depth = depth * 2 ** (i - 1)
+            self.decoder.add_module(f'pyramid_{i_depth}-{o_depth}_conv',
+                                    nn.ConvTranspose2d(i_depth, o_depth, filt_size, stride, pad, bias=True))
+            self.decoder.add_module(f'pyramid_{o_depth}_batchnorm',
+                                    nn.BatchNorm2d(o_depth))
+            self.decoder.add_module(f'pyramid_{o_depth}_relu', nn.ReLU(inplace=True))
+        
+        # Final transposed convolution to return to img_size
+        # Final activation is tanh instead of relu to allow negative pixel output
+        self.decoder.add_module('output-conv', nn.ConvTranspose2d(depth, n_channels,
+                                                                  filt_size, stride, pad, bias=True))
+        self.decoder.add_module('output-tanh', nn.Tanh())
+
+        # Model weights init
+        ####################
+        # Randomly initialize the model weights using kaiming method
+        # Reference: "Delving deep into rectifiers: Surpassing human-level
+        # performance on ImageNet classification" - He, K. et al. (2015)
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, nn.BatchNorm2d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+
+    def encode(self, imgs):
+        """
+        Encode the images into latent space vectors (mean and log variance representation)
+        input:  imgs   [batch_size, 3, 256, 256]
+        output: mu     [batch_size, n_latent, 1, 1]
+                logvar [batch_size, n_latent, 1, 1]
+        """
+        output = self.encoder(imgs)
+        output = output.squeeze(-1).squeeze(-1)
+        return [self.conv_mu(output), self.conv_logvar(output)]
+
+    def generate(self, mu, logvar):
+        """
+        Generates a random latent vector using the trained mean and log variance representation
+        input:  mu     [batch_size, n_latent, 1, 1]
+                logvar [batch_size, n_latent, 1, 1]
+        output: gen    [batch_size, n_latent, 1, 1]
+        """
+        std = torch.exp(0.5 * logvar)
+        gen = torch.randn_like(std)
+        return gen.mul(std).add_(mu)
+
+    def decode(self, gen):
+        """
+        Restores an image representation from the generated latent vector
+        input:  gen      [batch_size, n_latent, 1, 1]
+        output: gen_imgs [batch_size, 3, 256, 256]
+        """
+        return self.decoder(gen)
+
+    def forward(self, imgs):
+        """
+        Generates reconstituted images from input images based on learned representation
+        input: imgs     [batch_size, 3, 256, 256]
+        ouput: gen_imgs [batch_size, 3, 256, 256]
+               mu       [batch_size, n_latent]
+               logvar   [batch_size, n_latent]
+        """
+        mu, logvar = self.encode(imgs)
+        gen = self.generate(mu, logvar)
+        for tensor in (mu, logvar):
+            tensor = tensor.squeeze(-1).squeeze(-1)
+        return self.decode(gen), mu, logvar
+
+    
+class VAE2DLoss(nn.Module):
+
+    def __init__(self, kl_weight=1):
+        super(VAE2DLoss, self).__init__()
+        self.kl_weight = kl_weight
+
+    def forward(self, gen_imgs, imgs, mu, logvar, reduce=True):
+        """
+        input:  gen_imgs [batch_size, n_channels, img_size, img_size]
+                imgs     [batch_size, n_channels, img_size, img_size]
+                mu       [batch_size, n_latent]
+                logvar   [batch_size, n_latent]
+        output: loss      scalar (-ELBO)
+                loss_desc {'KL': KL, 'logp': gen_err}
+        """
+        batch_size = imgs.shape[0]
+        gen_err = (imgs - gen_imgs).pow(2).reshape(batch_size, -1)
+        gen_err = 0.5 * torch.sum(gen_err, dim=-1)
+        if reduce:
+            gen_err = torch.mean(gen_err)
+
+        # KL(q || p) = -log_sigma + sigma^2/2 + mu^2/2 - 1/2
+        KL = (-logvar + logvar.exp() + mu.pow(2) - 1) * 0.5
+        KL = torch.sum(KL, dim=-1)
+        if reduce:
+            KL = torch.mean(KL)
+
+        loss = gen_err + self.kl_weight * KL
+        return loss, {'KL': KL, 'logp': -gen_err}
+
+    
+def load_datasets(data_path, img_size, batch_size=32):
+    """
+    Load the image datasets from vae_train and vae_test
+    Transform to correct image size
+    """
+    data_path = Path(data_path)
+    train_path = data_path / 'train/train/'
+    val_path = data_path / 'train/val/'
+    test_path = data_path / 'test/'
+    
+    norm_args = {'mean': [0.5] * n_channels,
+                 'std': [0.5] * n_channels}
+    jitter_args = {'brightness': 0.1,
+                   'contrast': 0.1,
+                   'saturation': 0.1}  # hue unchanged
+    
+    train_transform = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.RandomCrop(img_size),          # vary horizontal position
+        transforms.RandomHorizontalFlip(p=0.25),  # vary photo orientation
+        transforms.RandomVerticalFlip(p=0.25),
+        transforms.ColorJitter(**jitter_args),    # vary photo lighting
+        transforms.ToTensor(),
+        transforms.Normalize(**norm_args)])
+    
+    test_transform = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.CenterCrop(img_size),  # assume center is most important
+        transforms.ToTensor(),
+        transforms.Normalize(**norm_args)])
+
+    train_ds = datasets.ImageFolder(train_path, train_transform)
+    val_ds = datasets.ImageFolder(val_path, test_transform)
+    test_ds = datasets.ImageFolder(test_path, test_transform)
+    
+    
+    loader_args = {'shuffle': True,
+                   'num_workers': 4}
+    train_dl = torch.utils.data.DataLoader(train_ds, batch_size=batch_size, **loader_args)
+    val_dl = torch.utils.data.DataLoader(val_ds, batch_size=batch_size, **loader_args)
+    test_dl = torch.utils.data.DataLoader(test_ds, batch_size=1, ** loader_args)
+    
+    return train_dl, val_dl, test_dl
---
+++ demo.py
@@ -0,0 +1,109 @@
+from VAE2D import *
+import matplotlib.pyplot as plt
+
+def load_checkpoint(model, device):
+    checkpoint = torch.load('models/190124-skin/best_model.pth.tar', map_location=device)
+    model.load_state_dict(checkpoint['state_dict'])
+
+    print("Checkpoint Performance:")
+    print(f"Validation loss: {checkpoint['val_loss']:.3f}")
+    print(f"Epoch: {checkpoint['epoch']}")
+    
+    return model
+
+
+def get_random_images(dl, n=20):
+    for i, (X, y) in enumerate(dl):
+        if i == 0:
+            imgs = X
+            targets = y
+        elif i < n:
+            imgs = torch.cat([imgs, X], 0)
+            targets = torch.cat([targets, y], 0)
+        else:
+            break
+    return imgs, targets
+
+
+def list_target_classes(dl):
+    classes = dl.dataset.classes
+    for i, clss in enumerate(classes):
+        print(f"{i} = {clss}")
+    
+    
+def show_images(imgs):
+    # Scale images back to 0-1
+    imgs = (imgs + 1) / 2
+    grid = make_grid(imgs, nrow=5, padding=20).cpu()
+    plt.imshow(grid.permute(1, 2, 0))
+
+
+# Unchanged
+def _log_mean_exp(x, dim):
+    """
+    A numerical stable version of log(mean(exp(x)))
+    :param x: The input
+    :param dim: The dimension along which to take mean with
+    """
+    # m [dim1, 1]
+    m, _ = torch.max(x, dim=dim, keepdim=True)
+
+    # x0 [dm1, dim2]
+    x0 = x - m
+
+    # m [dim1]
+    m = m.squeeze(dim)
+
+    return m + torch.log(torch.mean(torch.exp(x0),
+                                    dim=dim))
+
+def get_iwae_score(vae, image, L=5):
+    """
+    The vae score for a single image, which is basically the loss
+    :param image: [1, 3, 256, 256]
+    :return scocre: (iwae score, iwae KL, iwae reconst).
+    """
+    # [L, 3, 256, 256]
+    image_batch = image.expand(L,
+                               image.size(1),
+                               image.size(2),
+                               image.size(3))
+
+    # [L, z_dim, 1, 1]
+    mu, logvar = vae.encode(image_batch)
+    eps = torch.randn_like(mu)
+    z = mu + eps * torch.exp(0.5 * logvar)
+    kl_weight = criterion.kl_weight
+    # [L, 3, 256, 256]
+    reconst = vae.decode(z)
+    # [L]
+    log_p_x_z = -torch.sum((reconst - image_batch).pow(2).reshape(L, -1),
+                          dim=1)
+
+    # [L]
+    log_p_z = -torch.sum(z.pow(2).reshape(L, -1), dim=1)
+
+    # [L]
+    log_q_z = -torch.sum(eps.pow(2).reshape(L, -1), dim=1)
+
+    iwae_score = -_log_mean_exp(log_p_x_z + (log_p_z - log_q_z)*kl_weight, dim=0)
+    iwae_KL_score = -_log_mean_exp(log_p_z - log_q_z, dim=0)
+    iwae_reconst_score = -_log_mean_exp(log_p_x_z, dim=0)
+
+    return iwae_score, iwae_KL_score, iwae_reconst_score
+
+
+def compute_all_scores(vae, image):
+    """
+    Given an image compute all anomaly score
+    return (reconst_score, vae_score, iwae_score)
+    """
+    vae_loss, KL, reconst_err = get_vae_score(vae, image=image, L=15)
+    iwae_loss, iwae_KL, iwae_reconst = get_iwae_score(vae, image, L=15)
+    result = {'reconst_score': reconst_err.item(),
+              'KL_score': KL.item(),
+              'vae_score': vae_loss.item(),
+              'iwae_score': iwae_loss.item(),
+              'iwae_KL_score': iwae_KL.item(),
+              'iwae_reconst_score': iwae_reconst.item()}
+    return result
