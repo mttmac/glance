3a4e859372d703a632ad2ecbdd56e8670d5f440f
Matt MacDonald <mattmacdc@gmail.com>
2019-02-04 03:32:34 +0000

    Refine plots and fine tune setting of initial threshold

--- src/clustering.py
+++ src/clustering.py
@@ -1,8 +1,12 @@
 from VAE1D import *
 
+import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
+plt.style.use('ggplot')
+
 from sklearn.metrics import roc_auc_score, f1_score
-from itertools import product
-import scipy.stats as stats
+from sklearn.decomposition import PCA
+from scipy.stats import multivariate_normal
 
 size = 512
 n_channels = 14
@@ -113,3 +117,77 @@ def compute_latent_and_loss(dl, model, criterion):
     error = np.array(error)
     targets = np.array(targets)
     return latents, kl, error, targets
+
+
+# from pdb import set_trace
+def draw_ellipse(position, covariance, pca, **kwargs):
+    """Draw an ellipse with a given position and covariance"""
+    ax = plt.gca()
+    
+    # Convert covariance to principal axes
+    position = pca.transform(position[None, :]).ravel()
+    covariance = pca.transform(covariance)
+    covariance = pca.transform(covariance.T)
+    U, s, Vt = np.linalg.svd(covariance)
+    angle = np.degrees(np.arctan2(U[1, 0], U[0, 0]))
+    width, height = 2 * np.sqrt(s)
+    
+    # Draw the Ellipse
+    for nsig in range(1, 4):
+        ax.add_patch(Ellipse(position,
+                             nsig * width,
+                             nsig * height,
+                             angle,
+                             **kwargs))
+        
+def plot_clusters(latents, gmm, show_pdf=True, ax=None):
+    # Project to 2D for plotting
+    if ax is None:
+        plt.figure()
+        ax = plt.gca()
+    
+    pca = PCA(n_components=2)
+    lat_pca = pca.fit_transform(latents)
+    clusters = gmm.predict(latents)
+    
+    ax.scatter(lat_pca[:, 0], lat_pca[:, 1],
+               c=clusters, cmap='viridis',
+               zorder=2)
+    ax.axis('equal')
+    
+    if show_pdf:
+        w_max = gmm.weights_.max()
+        for pos, covar, w in zip(gmm.means_, gmm.covariances_, gmm.weights_):
+            draw_ellipse(pos, covar, pca, alpha=0.2 * w / w_max)
+
+            
+def calculate_log_probs(latents, gmm, method='bounded'):
+    # Manually calculate the probability of each point belonging to a cluster
+    means = gmm.means_
+    covars = gmm.covariances_
+    weights = gmm.weights_
+    
+    if method == 'bounded':
+        # Higher numerical precision but only returns the lower bound on probability
+        log_probs = np.zeros([latents.shape[0], means.shape[0]])
+        for k in range(means.shape[0]):
+            # Note: in log space addition is multiplication
+            log_probs[:, k] = multivariate_normal.logpdf(latents,
+                                                         mean=means[k, :],
+                                                         cov=covars[k, :, :])
+        log_probs = log_probs.max(axis=1)
+    elif method == 'clipped':
+        # Clipped on low end due to numerical round off but uses sum of all cluster probabilities
+        probs = np.zeros(latents.shape[0])
+        for k in range(means.shape[0]):
+            w = weights[k] / weights.max()
+            probs += w * multivariate_normal.pdf(latents,
+                                                 mean=means[k, :],
+                                                 cov=covars[k, :, :])
+        # Set all zero probabilities to min non-zero probability
+        probs[probs == 0] = probs[probs != 0].min()
+        log_probs = np.log(probs)
+    else:
+        print("Method not recognized, must be 'bounded' or 'clipped'.")
+    
+    return log_probs
